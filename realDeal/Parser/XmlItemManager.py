f663c81c5f8d18f1e8cb1ffcdf3b8479ceba891e
Ali Ismail <aismail102@gmail.com>
2021-10-01 17:44:52 -0400

    Fixing the update to accommodate percentages for types

--- realDeal/Parser/XmlItemManager.py
+++ realDeal/Parser/XmlItemManager.py
@@ -34,9 +34,23 @@ class XmlItemManager():
         return len(self._items)
 
     def _update(self):
-        sum = 0.0
+        sum     = 0.0
+
+        # Iterate through items to find the total sum
         for item in self._items:
             sum = sum + item.value
+
+        # Iterate through items to find what other items have the same type
+        # Generate a sum based on the total items 
+        for currItem in self._items:
+
+            # Start with a sum that is - the current items value, since we will add it to the sum
+            # when we iterate through the list looking for items with the same type
+            typeSum = -1 * currItem.value
+            for testItem in self._items:
+                if currItem.type == testItem.type:
+                    typeSum = typeSum + testItem.value
+                    
 
         for item in self._items:
             item.percent = (item.value / sum) * 100.00
